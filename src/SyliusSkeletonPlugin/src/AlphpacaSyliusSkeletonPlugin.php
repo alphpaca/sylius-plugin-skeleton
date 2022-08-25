482e65af74ecfc85c29e16b072ca1f4777eb4594
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2022-08-26 01:10:41 +0200

    Update Plugin definition

--- src/SyliusSkeletonPlugin/src/AlphpacaSyliusSkeletonPlugin.php
+++ src/SyliusSkeletonPlugin/src/AlphpacaSyliusSkeletonPlugin.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace Alphpaca\SyliusSkeletonPlugin;
 
-final class AlphpacaSyliusSkeletonPlugin
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class AlphpacaSyliusSkeletonPlugin extends Bundle
 {
+    use SyliusPluginTrait;
+
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
