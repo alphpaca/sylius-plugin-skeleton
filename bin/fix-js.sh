0595bf440459e8eb0f478dde13d5d62048eae19c
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2022-08-26 08:24:51 +0200

    Adjust fix-js script

--- bin/fix-js.sh
+++ bin/fix-js.sh
@@ -4,7 +4,9 @@ CHECK_OS="`uname -s`"
 if [[ "$CHECK_OS" = "Darwin" ]]; then
     sed -i '' 's/;;/;/g' node_modules/semantic-ui-css/semantic.min.css
     sed -i '' 's/;;/;/g' node_modules/semantic-ui-css/semantic.css
+    sed -i '' 's/Chart.min/chart.min/g' vendor/sylius/sylius/src/Sylius/Bundle/AdminBundle/Resources/private/js/sylius-chart.js
 else
     sed -i 's/;;/;/g' node_modules/semantic-ui-css/semantic.min.css
     sed -i 's/;;/;/g' node_modules/semantic-ui-css/semantic.css
+    sed -i 's/Chart.min/chart.min/g' vendor/sylius/sylius/src/Sylius/Bundle/AdminBundle/Resources/private/js/sylius-chart.js
 fi
